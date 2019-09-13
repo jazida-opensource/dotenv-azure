ec44c093b2a56886f93b1abc65edde99f4b98cfc
Daniel Sousa <sousa.dfs@gmail.com>
2019-09-12 21:48:18 -0300

    Fix error name

--- src/dotenv-azure.ts
+++ src/dotenv-azure.ts
@@ -184,4 +184,4 @@ export default class DotenvAzure {
   }
 }
 
-export { dotenv, DotenvAzure, MissingEnvVarsError, InvalidKeyVaultUrlError as InvalidKeyVaultUrl }
+export { dotenv, DotenvAzure, MissingEnvVarsError, InvalidKeyVaultUrlError }
