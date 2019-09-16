2abb93b044d93f524b5db0e6b8aa17d6740da85d
Daniel Sousa <sousa.dfs@gmail.com>
2019-09-16 18:38:52 -0300

    fix: don't throw on preload

--- config-safe.js
+++ config-safe.js
@@ -2,9 +2,12 @@ const forceSync = require('sync-rpc')
 const { populateProcessEnv } = require('./dist/lib/utils')
 const configSync = forceSync(require.resolve('./config-rpc'))
 
-const { parsed } = configSync({
-  safe: true,
-  allowEmptyValues: true
-})
-
-populateProcessEnv(parsed)
+try {
+  const { parsed } = configSync({
+    safe: true,
+    allowEmptyValues: true
+  })
+  populateProcessEnv(parsed)
+} catch {
+  // noop
+}
--- config.js
+++ config.js
@@ -2,5 +2,9 @@ const forceSync = require('sync-rpc')
 const { populateProcessEnv } = require('./dist/lib/utils')
 const configSync = forceSync(require.resolve('./config-rpc'))
 
-const { parsed } = configSync()
-populateProcessEnv(parsed)
+try {
+  const { parsed } = configSync()
+  populateProcessEnv(parsed)
+} catch {
+  // noop
+}
